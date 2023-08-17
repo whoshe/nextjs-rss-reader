577ea5cde9508bd57e95101439f2fbd463968ea9
Changwoo Nam <whoshe@funnyplan.com>
2023-08-17 12:11:56 +0900

    Pages deploy2

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   swcMinify: true,
   compiler: {
     styledComponents: true,
   },
+  basePath: "/nextjs-rss-reader",
   reactStrictMode: true,
 };
 
