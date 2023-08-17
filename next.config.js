b37f914114c6312a9a8eb4822df278e16c71a6d3
Changwoo Nam <whoshe@funnyplan.com>
2023-08-17 11:56:29 +0900

    Pages deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
