0dbc3eab5cc0b7cedb8e5f3005fc12c27d98aab3
Changwoo Nam <whoshe@funnyplan.com>
2024-02-23 15:16:24 +0900

    동네서점지도 뉴스 피드 추가

--- src/lib/rss.js
+++ src/lib/rss.js
@@ -1,21 +1,26 @@
-import Parser from "rss-parser";
+import Parser from 'rss-parser'
 
 export const FEEDS = [
   {
-    slug: "google-news",
-    title: "지역사랑상품권 또는 지역화폐 뉴스",
-    url: "https://news.google.com/rss/search?q=지역사랑상품권 OR 지역화폐&ceid=KR:ko&hl=ko&gl=KR",
+    slug: 'bookshopmap-blog',
+    title: '오늘의동네서점 블로그',
+    url: 'https://blog.bookshopmap.com/rss',
   },
   {
-    slug: "bookshopmap-blog",
-    title: "오늘의동네서점 블로그",
-    url: "https://blog.bookshopmap.com/rss",
+    slug: 'google-news',
+    title: '지역화폐 뉴스 기사',
+    url: 'https://news.google.com/rss/search?q=지역사랑상품권 OR 지역화폐&ceid=KR:ko&hl=ko&gl=KR',
   },
-];
+  {
+    slug: 'bookshopmap-news',
+    title: '동네서점지도 뉴스 기사',
+    url: 'https://news.google.com/rss/search?q=주식회사 동네서점 OR 동네서점지도&ceid=KR:ko&hl=ko&gl=KR',
+  },
+]
 
 export async function getFeed(feedUrl) {
-  let parser = new Parser();
-  let feed = await parser.parseURL(encodeURI(feedUrl));
+  let parser = new Parser()
+  let feed = await parser.parseURL(encodeURI(feedUrl))
 
-  return feed;
+  return feed
 }
